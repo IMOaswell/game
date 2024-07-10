539dc42d7223e7c1e494abe4bc46a276a763854a
IMOitself <russelljonnbautista@gmail.com>
2024-07-10 14:54:45 +0800

    ●style: format code and rearrange imports

--- java/imo/game/MainActivity.java
+++ java/imo/game/MainActivity.java
@@ -2,39 +2,39 @@ package imo.game;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
-import android.view.MotionEvent;
 
 public class MainActivity extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
+
         View root = findViewById(R.id.root);
         root.setBackgroundResource(R.drawable.card_transparent);
         root.setOnTouchListener(new OnTouchListener(){
-            final int CENTER_NO_TOUCH_AREA = 75;
-            @Override
-            public boolean onTouch(View v, MotionEvent motion){
-                if(MotionEvent.ACTION_MOVE == motion.getAction()){
-                    int viewCenter = v.getWidth() / 2;
-                    float x = motion.getX();
-                    int resId = -1;
-                    
-                    x -= viewCenter;
-                        
-                    if(x < -CENTER_NO_TOUCH_AREA) resId = R.drawable.card_left;
-                    if(x > CENTER_NO_TOUCH_AREA) resId = R.drawable.card_right;
-                    if(resId == -1) resId = R.drawable.card_solid;
-                    v.setBackgroundResource(resId);
+                final int CENTER_NO_TOUCH_AREA = 75;
+                @Override
+                public boolean onTouch(View v,MotionEvent motion){
+                    if(MotionEvent.ACTION_MOVE == motion.getAction()){
+                        int viewCenter = v.getWidth() / 2;
+                        float x = motion.getX();
+                        int resId = -1;
+
+                        x -= viewCenter;
+
+                        if(x < -CENTER_NO_TOUCH_AREA) resId = R.drawable.card_left;
+                        if(x > CENTER_NO_TOUCH_AREA) resId = R.drawable.card_right;
+                        if(resId == -1) resId = R.drawable.card_solid;
+                        v.setBackgroundResource(resId);
+                    }
+                    if(MotionEvent.ACTION_UP == motion.getAction()){
+                        v.setBackgroundResource(R.drawable.card_transparent);
+                    }
+                    return true;
                 }
-                if(MotionEvent.ACTION_UP == motion.getAction()){
-                    v.setBackgroundResource(R.drawable.card_transparent);
-                }
-                return true;
-            }
-        });
+            });
     }
 }
