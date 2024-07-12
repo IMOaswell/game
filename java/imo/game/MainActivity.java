403ef3414c6e660d188104e7ecee358da6d07b6b
IMOitself <russelljonnbautista@gmail.com>
2024-07-12 12:34:53 +0800

    style: format code style and organize imports

--- java/imo/game/MainActivity.java
+++ java/imo/game/MainActivity.java
@@ -5,47 +5,47 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
-import android.widget.TextView;
-import java.util.List;
-import java.util.Arrays;
 import android.widget.ImageView;
+import android.widget.TextView;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity{
     List<String> storyScript;
     int scriptIndex = 0;
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
+
         View root = findViewById(R.id.root);
         final TextView textview = findViewById(R.id.textview);
         final ImageView imageview = findViewById(R.id.imageview);
         String story_script = getString(R.string.story_script);
         story_script = story_script.trim();
         storyScript = Arrays.asList(story_script.split("\n"));
-        
+
         root.setOnTouchListener(new OnTouchListener(){
                 @Override
                 public boolean onTouch(View v,MotionEvent motion){
                     if(MotionEvent.ACTION_DOWN != motion.getAction()) return true;
                     if(scriptIndex > storyScript.size() - 1) return true;
-                    
+
                     String string = storyScript.get(scriptIndex);
                     scriptIndex++;
-                    
+
                     boolean isCommand = string.startsWith(Command.PREFIX);
                     if(!isCommand)
                         textview.setText(string);
-                        
+
                     if(Command.isCommand(Command.DISPLAY_CHOICES, string)){
                         Command.runDisplayChoices(string, textview);
                     }
                     return true;
                 }
             });
-        
+
         imageview.setImageResource(R.drawable.card_transparent);
         imageview.setOnTouchListener(new OnTouchListener(){
                 final int CENTER_NO_TOUCH_AREA = 75;
@@ -77,24 +77,26 @@ public class MainActivity extends Activity{
                 }
             });
     }
-    
+
     void triggerYes(){
         //do something
     }
-    
+
     void triggerNo(){
         //do something
     }
-    
+
     static class Command{
         final static String PREFIX = "/";
         final static String DISPLAY_CHOICES = PREFIX + "choices";
-        static boolean isCommand(String command, String input){
+        
+        static boolean isCommand(String command,String input){
             return input.startsWith(command);
         }
-        static void runDisplayChoices(String string, TextView textview){
+
+        static void runDisplayChoices(String string,TextView textview){
             string = string.substring((DISPLAY_CHOICES + '=').length());
-            
+
             String noString = string.substring(0, string.indexOf("::")).trim();
             String yesString = string.substring(string.indexOf("::") + 1).trim();
             textview.append("\n");
