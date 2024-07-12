a4f4bbb116eaeb515825543eb2bfaf1f034a9266
IMOitself <russelljonnbautista@gmail.com>
2024-07-12 10:55:23 +0800

    feat: activate card swiping left or right to select  no or yes when command run choices

--- java/imo/game/MainActivity.java
+++ java/imo/game/MainActivity.java
@@ -10,8 +10,11 @@ import java.util.List;
 import java.util.Arrays;
 
 public class MainActivity extends Activity{
+    View root;
     TextView textview;
+    List<String> storyScript;
     int scriptIndex = 0;
+    OnTouchListener shuffleScriptMode;
     
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -19,75 +22,74 @@ public class MainActivity extends Activity{
         setContentView(R.layout.main);
         
         textview = findViewById(R.id.textview);
-        View root = findViewById(R.id.root);
+        root = findViewById(R.id.root);
         String story_script = getString(R.string.story_script);
-        List<String> storyScript;
-        
         story_script = story_script.trim();
         storyScript = Arrays.asList(story_script.split("\n"));
         
-        final List<String> script = storyScript;
-        
-        root.setOnTouchListener(new OnTouchListener(){
-                @Override
-                public boolean onTouch(View v,MotionEvent motion){
-                    if(MotionEvent.ACTION_DOWN != motion.getAction()) return true;
-                    if(scriptIndex > script.size() - 1) return true;
-                    
-                    String string = script.get(scriptIndex);
-                    scriptIndex++;
-                    
-                    boolean isCommand = string.startsWith(Command.PREFIX);
-                    if(!isCommand)
-                        textview.setText(string);
-                        
-                    if(Command.isCommand(Command.CHOICES, string)){
-                        Command.runChoices(string, textview);
-                    }
-                    return true;
+        shuffleScriptMode = new OnTouchListener(){
+            @Override
+            public boolean onTouch(View v,MotionEvent motion){
+                if(MotionEvent.ACTION_DOWN != motion.getAction()) return true;
+                if(scriptIndex > storyScript.size() - 1) return true;
+
+                String string = storyScript.get(scriptIndex);
+                scriptIndex++;
+
+                boolean isCommand = string.startsWith(Command.PREFIX);
+                if(!isCommand)
+                    textview.setText(string);
+
+                if(Command.isCommand(Command.CHOICES, string)){
+                    Command.runChoices(string, textview);
+                    root.setOnTouchListener(choiceSelectMode());
                 }
-            });
-        
+                return true;
+            }
+        };
+        root.setOnTouchListener(shuffleScriptMode);
         root.setBackgroundResource(R.drawable.card_transparent);
-        
-        if(true) return;
-        
-        root.setOnTouchListener(new OnTouchListener(){
-                final int CENTER_NO_TOUCH_AREA = 75;
-                @Override
-                public boolean onTouch(View v,MotionEvent motion){
-                    if(MotionEvent.ACTION_MOVE == motion.getAction()){
-                        int viewCenter = v.getWidth() / 2;
-                        float x = motion.getX();
-                        int resId = -1;
-
-                        x -= viewCenter;
-
-                        if(x < -CENTER_NO_TOUCH_AREA){
-                            resId = R.drawable.card_left;
-                            triggerNo();
-                        } 
-                        if(x > CENTER_NO_TOUCH_AREA){
-                            resId = R.drawable.card_right;
-                            triggerYes();
-                        }
-                        if(resId == -1) resId = R.drawable.card_solid;
-                        v.setBackgroundResource(resId);
-                    }
-                    if(MotionEvent.ACTION_UP == motion.getAction()){
-                        v.setBackgroundResource(R.drawable.card_transparent);
-                    }
-                    return true;
-                }
-            });
     }
     
     void triggerYes(){
         textview.setText("Yes");
+        root.setOnTouchListener(shuffleScriptMode);
     }
     
     void triggerNo(){
         textview.setText("No");
+        root.setOnTouchListener(shuffleScriptMode);
+    }
+    
+    OnTouchListener choiceSelectMode(){
+        return new OnTouchListener(){
+            final int CENTER_NO_TOUCH_AREA = 75;
+            @Override
+            public boolean onTouch(View v,MotionEvent motion){
+                if(MotionEvent.ACTION_MOVE == motion.getAction()){
+                    int viewCenter = v.getWidth() / 2;
+                    float x = motion.getX();
+                    int resId = -1;
+
+                    x -= viewCenter;
+
+                    if(x < -CENTER_NO_TOUCH_AREA){
+                        resId = R.drawable.card_left;
+                        triggerNo();
+                    } 
+                    if(x > CENTER_NO_TOUCH_AREA){
+                        resId = R.drawable.card_right;
+                        triggerYes();
+                    }
+                    if(resId == -1) resId = R.drawable.card_solid;
+                    v.setBackgroundResource(resId);
+                }
+                if(MotionEvent.ACTION_UP == motion.getAction()){
+                    v.setBackgroundResource(R.drawable.card_transparent);
+                }
+                return true;
+            }
+        };
     }
     
     static class Command{
